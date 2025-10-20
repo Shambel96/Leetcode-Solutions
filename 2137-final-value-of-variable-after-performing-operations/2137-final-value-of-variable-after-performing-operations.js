068cf0d042419f440c341856ccf50dcbc9f8fc6b
Shambel Dechu <shambeldechu@gmail.com>
2025-10-21 01:24:37 +0300

    Time: 3 ms (8.1%), Space: 56.9 MB (10%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    let X = 0;
+    
+    for (let op of operations) {
+        if (op.includes('++')) {
+            X += 1;
+        } else if (op.includes('--')) {
+            X -= 1;
+        }
+    }
+    
+    return X;
+};
