c8549c998b994beb55f7d866af8b88b87fc16994
Shambel Dechu <shambeldechu@gmail.com>
2025-11-21 02:52:03 +0300

    Time: 32 ms (39.31%), Space: 54.7 MB (85.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,20 +1,14 @@
-const nums = [3, 5, 6, 7, 8, 80];
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function(nums, target) {
-    const indices =[];
-   for (let i = 0; i < nums.length; i++) {
-   for (let j = 0; j < nums.length; j++){
-    if (i === j){
-        continue;
-    } 
-    
-    if(nums[i] + nums[j] === target){
-           indices.push(i, j);
-return indices;
-    } 
-   }
-   }
-           return `No answer match with your target`;
-
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
 };
-
-twoSum(nums, 87);
